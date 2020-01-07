f8d42e946501219614a42209642db002569a580d
delapenamarissa19 <55451249+delapenamarissa19@users.noreply.github.com>
2020-01-07 16:08:07 +0800

    Update CPE-412-19-20-PROGASS-0M.cpp

--- CPE-412-19-20-PROGASS-0M.cpp
+++ CPE-412-19-20-PROGASS-0M.cpp
@@ -1,1 +1,37 @@
-https://code.sololearn.com/cbqCROhdpUKS/?ref=app
+#include <iostream>
+using namespace std;
+//I.D.#: 620457
+int main()
+{
+    int j=0, state=0;
+    int table[7][2]={{0,1},{1,2},{2,3},{3,4},{4,5},{5,6},{6,0}};
+    int input;
+    int flag=0;
+    char st[20];
+    cin>>st;
+    while(st[j]!='\0')
+    {
+    // get column for symbol
+    switch(st[j])
+    {
+    case 'a': input=0; break; //column
+    case 'b': input=1; break; //column
+    }    
+    state = table[state][input];
+    if (state==6){
+        flag=1;
+        break;
+    }
+    j++;
+    }
+    
+    if (flag==0){
+        cout<<"ACCEPTED";
+    }
+    else {
+        cout<<"NOT ACCEPTED";
+    }
+
+    return 0;
+    
+}
